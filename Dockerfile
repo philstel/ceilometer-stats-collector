0a2686a56f2e1ab350192eeb5018f0ed02240c0b
Phil Stelzer <phil@philstelzer.de>
2016-06-07 19:33:25 +0200

    Now serves a metrics page for prometheus db to consume

--- Dockerfile
+++ Dockerfile
@@ -6,17 +6,14 @@ ENV OS_USERNAME demo
 ENV OS_PASSWORD openstack
 ENV OS_TENANT_NAME demo
 ENV OS_AUTH_URL "http://10.0.0.20:5000/v2.0"
-ENV INFLUX_HOST influxdb.service.consul
-ENV INFLUX_PORT 8086
-ENV INFLUX_DB cluster_metrics
 ENV STACK_ID 12345
-ENV INTERVAL 10
+ENV FLASK_APP main.py
 
 ADD requirements.txt .
-
+RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
 ADD envdefault.py .
 ADD main.py .
 
-ENTRYPOINT ["python", "main.py"]
+ENTRYPOINT ["flask", "run", "--host=0.0.0.0"]
--- main.py
+++ main.py
@@ -5,36 +5,25 @@ from datetime import timedelta, datetime
 import argparse
 from envdefault import EnvDefault
 import threading
+from flask import Flask, Response
+import os
 
-parser = argparse.ArgumentParser()
-parser.add_argument("OS_USERNAME", action=EnvDefault, envvar='OS_USERNAME', help="Username to use for the ceilometer API")
-parser.add_argument("OS_PASSWORD", action=EnvDefault, envvar='OS_PASSWORD', help="Password to use for the ceilometer API")
-parser.add_argument("OS_TENANT_NAME", action=EnvDefault, envvar='OS_TENANT_NAME', help="Tenant name to use for the ceilometer API")
-parser.add_argument("OS_AUTH_URL", action=EnvDefault, envvar='OS_AUTH_URL', help="Auth url to use for the ceilometer API")
-parser.add_argument("INFLUX_HOST", action=EnvDefault, envvar='INFLUX_HOST', help="InfluxDB IP or Hostname")
-parser.add_argument("INFLUX_PORT", action=EnvDefault, envvar='INFLUX_PORT', help="InfluxDB Port")
-parser.add_argument("INFLUX_DB", action=EnvDefault, envvar='INFLUX_DB', help="InfluxDB Database to use")
-parser.add_argument("STACK_ID", action=EnvDefault, envvar='STACK_ID', help="Heat Stack ID to observe. Queries will use this to find Resources belonging to the stack.")
-parser.add_argument("INTERVAL", action=EnvDefault, envvar='INTERVAL', help="The interval to use for querying ceilometer and pushing the data to influx", type=int)
-args = parser.parse_args()
+app = Flask(__name__)
 
-print(datetime.utcnow())
+args = {
+    'OS_USERNAME': os.environ.get('OS_USERNAME'),
+    'OS_PASSWORD': os.environ.get('OS_PASSWORD'),
+    'OS_TENANT_NAME': os.environ.get('OS_TENANT_NAME'),
+    'OS_AUTH_URL': os.environ.get('OS_AUTH_URL'),
+    'STACK_ID': os.environ.get('STACK_ID')
+}
+
 cclient = ceilometerclient.client.get_client(2,
-                                             os_username=args.OS_USERNAME,
-                                             os_password=args.OS_PASSWORD,
-                                             os_tenant_name=args.OS_TENANT_NAME,
-                                             os_auth_url=args.OS_AUTH_URL)
+                                             os_username=args['OS_USERNAME'],
+                                             os_password=args['OS_PASSWORD'],
+                                             os_tenant_name=args['OS_TENANT_NAME'],
+                                             os_auth_url=args['OS_AUTH_URL'])
 
-influxdbConnectionString = 'http://%s:%s' % (args.INFLUX_HOST, args.INFLUX_PORT)
-
-# create influxdb database if not existent
-requests.post(influxdbConnectionString + '/query', data={'q': 'CREATE DATABASE %s' % args.INFLUX_DB})
-
-def insertCpuDataForInstance(instance):
-    requests.post(influxdbConnectionString + '/write?db=%s' % args.INFLUX_DB,
-                  data='cpu_load,host=%s,type=%s value=%s' % (instance.metadata['display_name'],
-                                                              instance.metadata.get('user_metadata.type', ""),
-                                                              instance.volume))
 # helper function
 def distinct(iterable, keyfunc=None):
     seen = set()
@@ -44,10 +33,11 @@ def distinct(iterable, keyfunc=None):
             seen.add(key)
             yield item
 
-def periodicCollect():
+@app.route('/metrics')
+def metrics():
     # check how many instances are alive in our cluster
-    activeInstancesQuery = [dict(field='metadata.user_metadata.stack', op='eq', value=args.STACK_ID),
-                            dict(field='timestamp', op='gt', value=datetime.utcnow() - timedelta(seconds=15))]
+    activeInstancesQuery = [dict(field='metadata.user_metadata.stack', op='eq', value=args['STACK_ID']),
+                            dict(field='timestamp', op='gt', value=datetime.utcnow() - timedelta(seconds=7))]
 
     activeInstances = cclient.statistics.list('cpu_util', q=activeInstancesQuery, groupby='resource_id')
     print("Aktive Instanzen: %s" % len(activeInstances))
@@ -55,15 +45,14 @@ def periodicCollect():
     #for instance in activeInstances:
     #    print("Instanz: %s, CPU Auslastung: %s %s" % (instance.groupby['resource_id'], instance.avg, instance.unit))
 
-    ### get all CPU usage data for those instances and post them to influx
-    allInstancesWithCpuUsageInStackQuery = [dict(field='metadata.user_metadata.stack', op='eq', value=args.STACK_ID),
+    ### get all CPU usage data for those instances
+    allInstancesWithCpuUsageInStackQuery = [dict(field='metadata.user_metadata.stack', op='eq', value=args['STACK_ID']),
                                             dict(field='meter', op='eq', value='cpu_util'),
-                                            dict(field='timestamp', op='gt', value=datetime.utcnow() - timedelta(seconds=15))]
+                                            dict(field='timestamp', op='gt', value=datetime.utcnow() - timedelta(seconds=7))]
 
     allInstancesWithCpuUsageInStack = cclient.new_samples.list(q=allInstancesWithCpuUsageInStackQuery, limit=100)
     for instance in allInstancesWithCpuUsageInStack:
         print("Instanz: %s, CPU Auslastung: %s %s" % (instance.metadata['display_name'], instance.volume, instance.unit))
-        insertCpuDataForInstance(instance)
 
     allSlavesWithDuplicates = filter(lambda i: i.metadata.get('user_metadata.type', "") == "slave", allInstancesWithCpuUsageInStack)
     allSlaves = distinct(allSlavesWithDuplicates, lambda slave: slave.resource_id)
@@ -76,10 +65,14 @@ def periodicCollect():
     print("Anzahl Slaves: %s" % numberOfSlaves)
     print("Anzahl Master: %s" % numberOfMasters)
 
-    # Insert Node count
-    requests.post(influxdbConnectionString + '/write?db=%s' % args.INFLUX_DB, data='slave_count value=%s' % numberOfSlaves)
-    requests.post(influxdbConnectionString + '/write?db=%s' % args.INFLUX_DB, data='master_count value=%s' % numberOfMasters)
+    data = ""
+    for instance in allInstancesWithCpuUsageInStack:
+        data += '\ncpu_load,host=%s,type=%s value=%s' % (instance.metadata['display_name'],
+                                                    instance.metadata.get('user_metadata.type', ""),
+                                                    instance.volume)
 
-    threading.Timer(args.INTERVAL, periodicCollect).start()
+    output = """slave_count %s
+master_count %s%s
+    """ % (numberOfSlaves, numberOfMasters, data)
 
-periodicCollect()
+    return Response(output, mimetype='text/plain')
